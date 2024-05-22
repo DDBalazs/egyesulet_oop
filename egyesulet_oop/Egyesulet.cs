683a4c023a5ec04eee7164f676384fb3827dbb8f
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2024-05-22 14:24:09 +0200

    Add project files.

---
+++ egyesulet_oop/Egyesulet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egyesulet_oop
+{
+    class Egyesulet
+    {
+        string nev;
+        string szulo;
+        int fo;
+        int osszeg;
+
+        private string Nev { get => nev; set => nev = value; }
+        private string Szulo { get => szulo; set => szulo = value; }
+        private int Fo { get => fo; set => fo = value; }
+        private int Osszeg { get => osszeg; set => osszeg = value; }
+
+        public Egyesulet(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            Nev = darabok[0];
+            Szulo = darabok[1];
+            Fo = Convert.ToInt32(darabok[2]);
+            Osszeg = Convert.ToInt32(darabok[3]);
+        }
+    }
+}
---
+++ egyesulet_oop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egyesulet_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Egyesulet> lista = new List<Egyesulet>();
+            FileStream fs = new FileStream("bevasarlolista.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            while (!sr.EndOfStream)
+            {
+                Egyesulet b = new Egyesulet(sr.ReadLine());
+                lista.Add(b);
+            }
+            sr.Close();
+            fs.Close();
+
+            int bef = 0;
+            for(int i = 0; i < lista.Count; i++)
+            {
+                bef += lista[i].Fo;
+            }        
+        
+        }
+    }
+}
